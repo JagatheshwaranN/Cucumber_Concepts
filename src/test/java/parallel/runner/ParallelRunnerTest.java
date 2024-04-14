3509e51978547daeafe960c8166ea8a63968af35
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-04-14 16:05:16 +0530

    Cucumber Note Update Completed.Serenity Report Scenario Added.

--- src/test/java/parallel/runner/ParallelRunnerTest.java
+++ src/test/java/parallel/runner/ParallelRunnerTest.java
@@ -5,10 +5,11 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = "src/test/resources/features/parallel/", glue = {"parallel.steps"},
-        plugin = { "pretty",
-        "json:target/cucumber/report/report.json", "junit:target/cucumber/report/report.xml",
-        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-        "timeline:target/test-output-thread/"},
+        plugin = {"pretty",
+                "json:target/cucumber/report/report.json", "junit:target/cucumber/report/report.xml",
+                "html:target/cucumber/report/report.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "timeline:target/test-output-thread/"},
         publish = true, monochrome = true)
 public class ParallelRunnerTest extends AbstractTestNGCucumberTests {
 
